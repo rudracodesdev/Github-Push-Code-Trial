38524651082ac21ec6a047d04c45f9ee8086b098
Rudra-Dev <rudrajena928@gmail.com>
2026-09-01 05:43:46 +0530

    Update console messages in reallyHelloWhat.js

--- reallyHelloWhat.js
+++ reallyHelloWhat.js
@@ -1,7 +1,7 @@
 console.log("Hello World?");
 console.log("I forgot how to push code into github again");
 console.log("Well. Well. Well.");
-console.log("I forgot most of the concepts I learnt bcoz-");
+console.log("I also forgot most of the concepts I learnt bcoz-");
 console.log("I am lazy to revise.");
 console.log("I studied almost most of the popular thing you can think about like-");
 console.log("DBMS, OOPS, SQL, HTML, CSS, DataAnalytics(Theory Only), Algo except-");
@@ -10,6 +10,8 @@ console.log("Crazy How I Learnt Basic of Languages like Java, C++, Python but re
 console.log("I have Set of targets to complete in 1st year.");
 console.log("It is more like growing forest but watering none of the Trees.");
 console.log("Looks like I need to change my way of learning. LETS-");
-console.log("");
-console.log("");
-console.log("");
+console.log("start REVISING instead of LEARNING.");
+console.log("It's a long race (say Marathon) so instead of Running fast from early and exhausting yourself-");
+console.log("it's better to slow some pace.");
+console.log("WELL THEN WISH ME GOOD LUCK EVERYONE WHO WATCH THIS.");
+console.log("Thank You and also I wrote this 17 times to share that I AM 17 YEAR OLD :) ")
