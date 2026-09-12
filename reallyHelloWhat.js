d51c716081a075ad365049e0599d363d7bc58455
Rudra-Dev <rudrajena928@gmail.com>
2026-09-12 20:48:02 +0530

    Remove pacing console log statement
    Removed a console log statement about pacing.

--- reallyHelloWhat.js
+++ reallyHelloWhat.js
@@ -12,6 +12,5 @@ console.log("It is more like growing forest but watering none of the Trees.");
 console.log("Looks like I need to change my way of learning. LETS-");
 console.log("start REVISING instead of LEARNING.");
 console.log("It's a long race (say Marathon) so instead of Running fast from early and exhausting yourself-");
-console.log("it's better to slow some pace.");
 
 
