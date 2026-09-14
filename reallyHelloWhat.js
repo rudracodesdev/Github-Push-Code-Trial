6f751ff2aa9a9b3b6fc5fa009ec5e911fef28859
Rudra-Dev <rudrajena928@gmail.com>
2026-09-14 22:49:39 +0530

    Remove console log about learning methods
    Removed a console log statement about changing learning methods.

--- reallyHelloWhat.js
+++ reallyHelloWhat.js
@@ -9,8 +9,6 @@ console.log("the king DSA. Maybe only Array Done.");
 console.log("Crazy How I Learnt Basic of Languages like Java, C++, Python but remember none");
 console.log("I have Set of targets to complete in 1st year.");
 console.log("It is more like growing forest but watering none of the Trees.");
-console.log("Looks like I need to change my way of learning. LETS-");
-
 
 
 
