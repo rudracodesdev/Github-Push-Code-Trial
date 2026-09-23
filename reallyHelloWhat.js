686977ad59563a161a9296d94f81efda5ce2ac7f
Rudra-Dev <rudrajena928@gmail.com>
2026-09-23 07:07:40 +0530

    Update reallyHelloWhat.js

--- reallyHelloWhat.js
+++ reallyHelloWhat.js
@@ -3,7 +3,7 @@ console.log("I forgot how to push code into github again");
 console.log("Well. Well. Well.");
 console.log("I also forgot most of the concepts I learnt bcoz-");
 console.log("I am lazy to revise.");
-console.log("I studied almost most of the popular thing you can think about like-");
+
 
 
 
